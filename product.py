5939dfc9566b6f5cf08becbec1780541e02a726d
Noel Mendoza <nmendozavd@gmail.com>
2019-05-15 15:19:40 -0700

    added read me file

--- product.py
+++ product.py
@@ -1,35 +1,48 @@
+
 class Product(object):
-    def __init__(self, price, item_name, weight, brand, cost):
+    def __init__ (self, price, item, weight, brand):
         self.price = price
-        self.item_name = item
+        self.item = item
         self.weight = weight
         self.brand = brand
-        self.cost = cost
-        self.status = "for sale"
+        self.status = "For Sale"
 
     def sell(self):
         self.status = "Sold"
         print self.status
+        return self  # chained methods
+        
 
     def add_tax(self, tax):
-        return self.price * .08
+        self.price = (self.price * tax) + self.price
+        
 
-    def return_item(self, reason, condition):
+    def return_reason(self, return_reason):
         self.return_reason = return_reason
-        self.condition = condition
 
-        if return_reason == "defective"
+        if return_reason == 'defective': 
             self.status = "defective"
             self.price = 0
+            return self  # chained methods
 
-        elif condition == "opened box"
-            self.status = "used"
-            self.price = self.price * .20
+        elif return_reason == 'like new':
+            self.status = 'for sale'
+            return self  # chained methods
+
+        elif return_reason == 'opened box':
+            self.status = 'used'
+            self.price *= .20   
+            return self #chained methods    
 
     def display_all(self):
-        print 'Price', str(self.price)
-        print 'Item Name', str(self.item_name)
-        print 'Weight', str(self.weight)
-        print 'Brand', str(self.brand)
-        print 'Cost', str(self.cost)
-        return self
+        print 'Price: ' + str(self.price)
+        print 'Item Name: ' + str(self.item)
+        print 'Weight: ' + str(self.weight) + 'lbs'
+        print 'Brand: ' + str(self.brand)
+        print 'Status: ' + str(self.status)
+        
+
+product1 = Product(100, 'iPhone', 2, 'Apple')
+product1.add_tax(.09)
+product1.return_reason('like new').sell()
+product1.display_all()
